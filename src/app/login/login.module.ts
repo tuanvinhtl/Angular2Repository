fcb12c5a6d9a5be6d698567ce2316523ae8dc27b
TuanVinh <tuanvinhtl@gmail.com>
2017-11-02 22:48:08 +0700

    Routes Lession

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { Routes, RouterModule } from '@angular/router';
+
+export const loginRoute:Routes=[
+  {path:'',component:LoginComponent}
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(loginRoute)
   ],
   declarations: [LoginComponent]
 })
