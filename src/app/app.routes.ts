48aa7dcf4c3a2f990191c8eea56ca1dffad48a5b
TuanVinh <tuanvinhtl@gmail.com>
2017-11-08 13:29:52 +0700

    Router Link Angular 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 export const appRoutes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', loadChildren: './login/login.module#LoginModule' },
-    { path: 'main', loadChildren: 'main/main.module#MainModule' }
+    { path: 'main', loadChildren: './main/main.module#MainModule' }
 ]
--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { Routes, RouterModule } from '@angular/router';
 
+export const userRoute: Routes = [
+  { path: '', component: UserComponent }
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(userRoute)
   ],
   declarations: [UserComponent]
 })
